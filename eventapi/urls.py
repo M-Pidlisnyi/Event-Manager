ec7c925a0b777d724fd2c69c2675aed417f8d5ba
M-Pidlisnyi <mishapropatriot@gmail.com>
2024-11-07 21:10:57 +0200

    CRUD for event model

--- eventapi/urls.py
+++ eventapi/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.EventListView.as_view(), name="event-list"),
-    path("event/view/<int:pk>", views.EventDetailView.as_view(), name="event-detail")
+    path("event/view/<int:pk>", views.EventDetailView.as_view(), name="event-detail"),
+    path("event/create", views.EventCreateView.as_view(), name="event-create"),
+    path("event/update/<int:pk>", views.EventUpdateView.as_view(), name="event-update"),
+    path("event/delete/<int:pk>", views.EventDeleteView.as_view(), name="event-delete"),
 
 ]
--- eventapi/views.py
+++ eventapi/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.http import HttpRequest
+from django.urls import reverse_lazy
 
 from .models import Event, Location, Organizer
+from .forms import EventForm
 
 # Create your views here.
 class EventListView(ListView):
@@ -9,3 +12,16 @@ class EventListView(ListView):
 
 class EventDetailView(DetailView):
     model = Event
+
+class EventCreateView(CreateView):
+    model = Event
+    form_class = EventForm
+
+class EventUpdateView(UpdateView):
+    model = Event
+    form_class = EventForm
+    
+class EventDeleteView(DeleteView):
+    model = Event
+    template_name = "eventapi/event_delete.html"
+    success_url = reverse_lazy("event-list")
